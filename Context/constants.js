a9ab7cd9247112b5a87ebba523245e5e18c72c4b
Gia2912 <jmryu525@gmail.com>
2022-12-31 21:40:45 +0700

    update web 3

--- Context/constants.js
+++ Context/constants.js
@@ -6,10 +6,10 @@ import transferFunds from "./TransferFunds.json";
 
 //NFT MARKETPLACE
 export const NFTMarketplaceAddress =
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  "0x4B2dbb1d76d10cce3A98444598B1b2d8A4938b83";
 export const NFTMarketplaceABI = nftMarketplace.abi;
 
 //TRANSFER FUNDS
 export const transferFundsAddress =
-  "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 export const transferFundsABI = transferFunds.abi;
